ade8fbbafba423e322bc896fb90c67d2bf1e2099
@eyobs <eyobs573@gmail.com>
2025-09-20 20:07:50 +0300

    added loading page

---
+++ app/quiz/loading.tsx
@@ -0,0 +1,10 @@
+import { Loader2 } from "lucide-react";
+import { Loader2Icon } from "lucide-react";
+
+export default function Loading() {
+    return (
+        <div className="flex items-center justify-center h-screen">
+            <Loader2Icon className="animate-spin" />
+        </div>
+    );
+}
